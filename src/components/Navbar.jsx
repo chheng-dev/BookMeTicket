fc1998fd68fc62d7aa30b8e7cdd7c3f787320eac
Chheng <oeungchungchheng2205@gmail.com>
2022-12-08 14:40:52 +0700

    remove dark mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,7 +67,7 @@ function Navbar() {
                         </div>
                     </div>
                     <div className="flex ">
-                        <a
+                        {/* <a
                             
                             className="flex items-center rounded text-gray-700 ml-2 lg:mt-0">
                             {
@@ -77,7 +77,7 @@ function Navbar() {
                                     <svg onClick={() => changeTheme("dark")} t="1669701413914" className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6162" width="20" height="20"><path d="M524.8 938.666667h-4.266667a439.893333 439.893333 0 0 1-313.173333-134.4 446.293333 446.293333 0 0 1-11.093333-597.333334 432.213333 432.213333 0 0 1 170.666666-116.906666 42.666667 42.666667 0 0 1 45.226667 9.386666 42.666667 42.666667 0 0 1 10.24 42.666667 358.4 358.4 0 0 0 82.773333 375.893333 361.386667 361.386667 0 0 0 376.746667 82.773334 42.666667 42.666667 0 0 1 54.186667 55.04A433.493333 433.493333 0 0 1 836.266667 810.666667a438.613333 438.613333 0 0 1-311.466667 128z" p-id="6163"></path></svg>
                                 )
                             }
-                        </a>
+                        </a> */}
                         <a href="#"
                             className="flex items-center text-md px-8 py-0.5 rounded text-gray-700 ml-2 lg:mt-0">Sign
                             in</a>
